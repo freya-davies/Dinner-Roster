633857b41b762762b4f7c7075c44235ee1e9b30d
Freya Davies <freyadavies1@gmail.com>
2019-11-03 11:53:14 +1300

    Add set up things

---
+++ routes/users.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const db = require('../db')
+const router = express.Router()
+
+
+// router.get('/', (req, res) => {
+//     res.render('index')
+// })
+
+// router.get('/profile/:id', (req, res) => {
+//     // let {id} = req.params
+
+//     res.render('profile')
+// })
+
+// router.get('/calendar', (req, res) => {
+//     res.render('calendar')
+// })
+
+// router.get('/dietaryRequirements', (req, res) => {
+//     res.render('dietaryRequirements')
+// })
+
+// router.get('/addUser', (req, res) => {
+//     res.render('addUser')
+// })
+
+// router.get('/addDayAway', (req, res) => {
+//     res.render('addDayAway')
+// })
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const userRoutes = require('./routes/users')
+const db = require('./db')
+
+
+const server = express()
+
+// Middleware
+
+server.set('view engine', 'hbs') // allow access to hbs file directory
+server.use(express.static('public')) // allow access to public folder
+server.use(express.urlencoded({extended: false})) //this lets us access form data 
+
+// Routes
+
+// server.get('/', (req, res) => {
+//     res.render('index')
+// })
+
+server.get('/', (req, res) => {
+    res.render('index')
+})
+
+server.get('/profile', (req, res) => {
+    res.send('Hello profiel page')
+})
+
+server.get('/profile/:id', (req, res) => {
+    // let {id} = req.params
+
+    res.render('profile')
+})
+
+server.get('/calendar', (req, res) => {
+    res.render('calendar')
+})
+
+server.get('/dietaryRequirements', (req, res) => {
+    res.render('dietaryRequirements')
+})
+
+server.get('/addUser', (req, res) => {
+    res.render('addUser')
+})
+
+server.get('/addDayAway', (req, res) => {
+    res.render('addDayAway')
+})
+
+
+module.exports = server
